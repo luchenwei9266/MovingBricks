e2c358296a25823b392b3b7d36bdd0a4f45f07f7
luchenwei9266 <luchenwei9266@163.com>
2017-09-25 18:04:35 +0800

    测试借口

--- src/components/coincard/CoinCard.js
+++ src/components/coincard/CoinCard.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { connect } from 'dva';
 import { Card } from 'antd';
 import CoinCardStyle from './CoinCard.css';
+import bittrex from '../../key';
 
-function CoinCardPage() {
-  return (
-    <Card style={{ width: 240 }} bodyStyle={{ padding: 0 }}>
+class CoinCardPage extends React.Component {
+  componentDidMount() {
+    bittrex.getmarketsummaries( function( data, err ) {
+      if (err) {
+        return console.error(err);
+      }
+      for( var i in data.result ) {
+        bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
+          console.log( ticker );
+        });
+      }
+    });
+  }
+  render() {
+    return (
+      <Card style={{ width: 240 }} bodyStyle={{ padding: 0 }}>
       <div className={CoinCardStyle.customImage}>
         <img alt="example" width="100%" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
       </div>
@@ -14,10 +28,11 @@ function CoinCardPage() {
         <p>www.instagram.com</p>
       </div>
     </Card>
-  );
+    )
+  }
 }
 
 CoinCardPage.propTypes = {
 };
 
-export default connect()(CoinCardPage);
+export default CoinCardPage;
